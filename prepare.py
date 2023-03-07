6049d7c06234b294f5f434c7fb86f7231105a39b
joshuamwo <joshuamuuo5@gmail.com>
2023-03-07 17:53:33 +0300

    added print statements for application progress feedback

--- prepare.py
+++ prepare.py
@@ -92,5 +92,5 @@ def prepare():
 	hist = model.fit(np.array(train_x), np.array(train_y), epochs = 200, batch_size = 5, verbose = 1)
 	#save model
 	model.save('chatbot_model.h5', hist)
-
+print("Data preparation complete")
 
